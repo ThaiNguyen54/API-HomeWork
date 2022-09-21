5ab7db5fafd3f4b9eda4d8b098adf8731abaeede
Thai Nguyen <59272463+ThaiNguyen54@users.noreply.github.com>
2022-09-21 11:58:33 +0700

    update

--- controllers/Message.js
+++ controllers/Message.js
@@ -1,9 +1,14 @@
+// To use require in ES module, you should follow these steps below.
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const myinfo = require("../data/myInfo.json");
+
 export const Hello = async (req,res) => {
     try{
-        // res.send("Hello, my name is Thai");
-        res.json({
-            'message': 'Hello, my name is Thai (19522188)'
-        })
+        // res.json({
+        //     'message': 'Hello, my name is Thai (19522188)'
+        // })
+        res.send(myinfo)
     } catch (error) {
         console.log(error);
     }
